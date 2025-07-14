d795eee6691a5f3ab7852be8fda2ac2f3d6cb3d8
nnhao131 <leesinmu9@gmail.com>
2025-07-14 09:17:20 +0700

    add AdminUI review, coupon manage

---
+++ AdminUI/Services/CouponServices/ICouponService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using AdminUI.Models.Coupon;
+
+namespace AdminUI.Services.CouponServices
+{
+
+        public interface ICouponService
+        {
+            Task<(IEnumerable<CouponViewModel> coupons, int totalCount)> GetAllAsync(
+                string searchTerm, bool? statusFilter, decimal? discountFilter, int page, int pageSize);
+            Task<CouponViewModel> GetByIdAsync(int id);
+            Task CreateAsync(CouponViewModel coupon);
+            Task UpdateAsync(int id, CouponViewModel coupon);
+            Task DeactivateAsync(int id);
+            Task ActivateAsync(int id);
+        }
+    }
---
+++ AdminUI/Services/ReviewServices/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdminUI.Services.ReviewServices
+{
+    public interface IReviewService
+    {
+        Task<(List<ReviewReadDto> Reviews, int TotalCount)> GetAllAsync(string? searchTerm, bool? statusFilter, int? bookFilter, int pageNumber, int pageSize);
+        Task<ReviewReadDto> GetByIdAsync(int id);
+        Task<ReviewReadDto> CreateAsync(ReviewCreateDto reviewDto);
+        Task UpdateAsync(int id, ReviewUpdateDto reviewDto);
+        Task DeleteAsync(int id);
+        Task<List<SelectListItem>> GetBookOptionsAsync();
+        Task<List<SelectListItem>> GetUserOptionsAsync();
+    }
+}
