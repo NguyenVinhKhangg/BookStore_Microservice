9d76539f44302ca3970faf25a4f74c04f1708770
Khang Nguyen <khangnvce170767@fpt.edu.vn>
2025-07-14 08:30:32 +0700

    feat: Book Client UI

---
+++ BookClient/Models/PageList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace BookClient.Models
+{
+    public class PageList<T>
+    {
+        [JsonPropertyName("items")]
+        public List<T> Items { get; set; }
+
+        [JsonPropertyName("totalCount")]
+        public int TotalCount { get; set; }
+
+        [JsonPropertyName("currentPage")]
+        public int CurrentPage { get; set; }
+
+        [JsonPropertyName("pageSize")]
+        public int PageSize { get; set; }
+
+        [JsonIgnore]
+        public int TotalPages
+            => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
